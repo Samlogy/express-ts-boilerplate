0a21f731889635c6e38ff03fd5aa32799d5457f8
Samy Senani <samy.senani@deepor.ai>
2022-04-21 15:37:52 +0200

    prisma knowledge

--- utils/connectPrisma.ts
+++ utils/connectPrisma.ts
@@ -30,8 +30,39 @@ async function main() {
     // console.log(deleteUser)
 
     // get all users
-    const users = await prisma.user.findMany()
+    const users = await prisma.user.findMany({
+        select: {
+            uuid: true,
+            name: true,
+            email: true,
+            createdAt: true,
+        },
+    })
     console.log(users)
+
+    // get one user
+    // try {
+    //     const data = {
+    //         name: 'sam',
+    //         email: 'sam@corp.com',
+    //         password: 'password',
+    //         createdAt: new Date(),
+    //     }
+    //     const userExist = await prisma.user.findUnique({
+    //         where: {
+    //             email: data.email,
+    //         },
+    //     })
+    //     if (!userExist) {
+    //         const newuser = await prisma.user.create({
+    //             data: data,
+    //         })
+    //     } else {
+    //         console.log('user already exist')
+    //     }
+    // } catch (err: any) {
+    //     console.log('err: ', err.message)
+    // }
 }
 
 const dbPrisma = main()
